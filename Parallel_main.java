af7b07d498defff1edc1fdf335f603923501ea68
mohamedAbdelety <48653305+mohamedAbdelety@users.noreply.github.com>
2019-12-18 06:17:41 +0200

    Update Parallel_main.java

--- Parallel_main.java
+++ Parallel_main.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package abd.el3atyanew;
+package PBFS;
 
 import java.util.Date;
 import java.util.LinkedList;
